70a50ec0086eb5436602dfbce76436a1530283c3
Marcelino <marcelinokenny12@gmail.com>
2024-10-23 21:27:11 +0700

    cleaning code

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    // Route::get('/protected', [ProtectedController::class, 'index'])->name('protected');
 });
 
-// Route::get('/login', [AuthController::class, 'login'])->name('login');
 
 
 
